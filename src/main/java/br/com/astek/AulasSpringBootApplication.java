34b1a1eabef930f05285159ed8ab11081f4680b5
Ricardo Oda <astek@astekinfo.com.br>
2018-09-18 12:25:57 -0300

    primeiro commit do projeto

---
+++ src/main/java/br/com/astek/AulasSpringBootApplication.java
@@ -0,0 +1,12 @@
+package br.com.astek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasSpringBootApplication.class, args);
+	}
+}
